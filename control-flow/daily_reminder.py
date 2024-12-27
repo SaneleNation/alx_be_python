8644ef4ac0a2060ca5b9f7c5404f1568b17eb3d8
SaneleNation <sanelezwa@gmail.com>
2024-12-27 08:36:50 -0800

    Changed if statement syntax

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -16,7 +16,7 @@ match priority :
     case _:
         print("Not a valid priority option")
 
-if "yes" in time:
+if time == "yes":
     print("Reminder:",task,chosen_task,"that requires immediate attention today!" )
 else:
-        print("Note:",task,chosen_task,".Consider completing it when you have free time.")
+    print("Note:",task,chosen_task,".Consider completing it when you have free time.")
