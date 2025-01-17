8b35ef457c05a89497c82617c9ef139b3d0f9ee0
SaneleNation <sanelezwa@gmail.com>
2025-01-17 08:58:20 -0800

    Rewrote script

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 # Define global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 9/5
-CELSIUS_TO_FAHRENHEIT_FACTOR = 5/9
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 # Function to convert Fahrenheit to Celsius
 def convert_to_celsius(fahrenheit):
@@ -29,3 +29,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
