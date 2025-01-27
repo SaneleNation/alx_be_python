fc4057222d991e80436cd574d346c981a3d6e39c
SaneleNation <sanelezwa@gmail.com>
2025-01-27 02:36:56 -0800

    Removed qoutations

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,11 +7,11 @@ class Book:
 
     def __del__(self):
         """Destructor that prints a message when a Book object is deleted."""
-        print(f"Deleting '{self.title}'")
+        print(f"Deleting {self.title}")
 
     def __str__(self):
         """String representation of the book."""
-        return f"'{self.title}' by {self.author}, published in {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
         """Official representation of the book for debugging or recreation."""
